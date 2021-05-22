449146f65ef3b2a95be3c1e4ab9f846562db672d
Colin Schlecht <schlechtcolin@yahoo.com>
2021-05-21 19:55:02 -0700

    done

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,4 +1,44 @@
 require('dotenv').config();
 const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/HelloWorldNFT.sol/HelloWorldNFT.json"); 
+/// console.log(JSON.stringify(contract.abi));
+// to print ABI to console, run: node scripts/mint-nft.js
+
+const contractAddress = process.env.contract;
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+  
+    //the transaction
+    const tx = {
+      'from': PUBLIC_KEY,
+      'to': contractAddress,
+      'nonce': nonce,
+      'gas': 500000,
+      'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+    };
+
+
+    const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+    signPromise.then((signedTx) => {
+  
+      web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(err, hash) {
+        if (!err) {
+          console.log("The hash of your transaction is: ", hash, "\nCheck Alchemy's Mempool to view the status of your transaction!"); 
+        } else {
+          console.log("Something went wrong when submitting your transaction:", err)
+        }
+      });
+    }).catch((err) => {
+      console.log(" Promise failed:", err);
+    });
+  }
+  
+  mintNFT("https://gateway.pinata.cloud/ipfs/Qmf8CEyZad6y7u9p3oZ8WCzeZ1HwcUKmMJ5UByKnwByUi7")
